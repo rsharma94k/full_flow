e21fe8f2e522549672155cb26c6754f83b605d8e
dev <rahul.bhrd@gmail.com>
2026-04-27 23:27:59 +0530

    code push

--- tests/test_api.py
+++ tests/test_api.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello World", "status": "deployed"}
+    assert response.json() == {"message": "Hello World"}
 
 def test_health_check():
     response = client.get("/health")
